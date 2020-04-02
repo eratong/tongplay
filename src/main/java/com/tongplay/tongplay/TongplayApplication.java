07bf9728db2a655f219a7fcbfc2b3411e4b35a8c
tong <tong>
2020-04-02 11:10:42 +0800

    初始化

---
+++ src/main/java/com/tongplay/tongplay/TongplayApplication.java
@@ -0,0 +1,13 @@
+package com.tongplay.tongplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tongplay/tongplay/TongplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongplay.tongplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
